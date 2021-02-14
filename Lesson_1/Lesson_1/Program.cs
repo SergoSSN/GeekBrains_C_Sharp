033621faa03ce953184d82e00b3674c58acf2524
SergoSSN <StavSergo26@yandex.ru>
2021-02-14 05:03:09 +0300

    Дз по 1 уроку

---
+++ Lesson_1/Lesson_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, как тебя зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет {name} [{DateTime.Now}] ");            
+        }
+    }
+}
