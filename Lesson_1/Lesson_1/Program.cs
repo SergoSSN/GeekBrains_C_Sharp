0c9edb3ab69fa16d9e1de8fc3e4197b003dde415
SergoSSN <StavSergo26@yandex.ru>
2021-02-14 06:27:38 +0300

    Дз-1 сдача

--- Lesson_1/Lesson_1/Program.cs
+++ Lesson_1/Lesson_1/Program.cs
@@ -6,9 +6,9 @@ namespace Lesson_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Привет, как тебя зовут?");
-            string name = Console.ReadLine();
-            Console.WriteLine($"Привет {name} [{DateTime.Now}] ");            
+            Console.WriteLine("Привет, как тебя зовут?");//Выводит данные для пользоваетля и переходит на новую строку
+            string name = Console.ReadLine();//водится имя пользователя и записывается в переменную name
+            Console.WriteLine($"Привет {name} [{DateTime.Now}] ");
         }
     }
 }
